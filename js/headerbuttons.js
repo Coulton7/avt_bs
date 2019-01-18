bc36691c63f7a94f375674d1f0a827e5cd0dd5a0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-01-18 12:43:45 +0000

    beautififed all the script

--- js/headerbuttons.js
+++ js/headerbuttons.js
@@ -1,30 +1,29 @@
 jQuery(document).ready(function($) {
-    var $analysisConsulting = $('.analysis-consulting-home-section');
-    var $onSiteServices = $('.on-site-services-home-section');
-    var $products = $('.products-home-section');
-    var $training = $('.training-home-section');
+  var $analysisConsulting = $('.analysis-consulting-home-section');
+  var $onSiteServices = $('.on-site-services-home-section');
+  var $products = $('.products-home-section');
+  var $training = $('.training-home-section');
 
-    $('#block-block-8 ul.nav-tabs li.tabs-left').click(function(){
-        var tab_id = $(this).attr('data-tab');
+  $('#block-block-8 ul.nav-tabs li.tabs-left').click(function() {
+    var tab_id = $(this).attr('data-tab');
 
 
-        $('#block-block-8 ul.nav-tabs li.tabs-left').removeClass('current');
-        $(this).addClass('current');
+    $('#block-block-8 ul.nav-tabs li.tabs-left').removeClass('current');
+    $(this).addClass('current');
 
-        $('#block-block-9 .tab-content').removeClass('current');
-        $('#block-block-9 .home-tab-image').removeClass('current');
+    $('#block-block-9 .tab-content').removeClass('current');
+    $('#block-block-9 .home-tab-image').removeClass('current');
 
-        $("#tab-"+tab_id).addClass('current');
-        $("#img-"+tab_id).addClass('current');
+    $("#tab-" + tab_id).addClass('current');
+    $("#img-" + tab_id).addClass('current');
 
-        // var bgColor = $('li#tab2.current').css('background-color');
+    // var bgColor = $('li#tab2.current').css('background-color');
 
-        if (tab_id != 1) {
-            $('#block-views-slick-carousel-block').css('display', 'none');
-        } else {
-            $('#block-views-slick-carousel-block').css('display', 'inherit');
-        }
+    if (tab_id != 1) {
+      $('#block-views-slick-carousel-block').css('display', 'none');
+    } else {
+      $('#block-views-slick-carousel-block').css('display', 'inherit');
+    }
 
-    });
-
+  });
 });
