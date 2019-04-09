15412f3cf8963198d741cf5bcc872db39efff058
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-09 12:30:40 +0100

    Adding key template commands to the avt_bs theme to keep it working

--- template.php
+++ template.php
@@ -1,6 +1,31 @@
 <?php
-
 /**
  * @file
  * The primary PHP file for this theme.
  */
+
+function avt_bs_preprocess_page(&$vars) {
+    // template files called page--contenttype.tpl.php
+    if (isset($vars['node']->type)) {
+        $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
+    }
+
+	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
+	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
+  drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/contact-icon.js');
+  drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/reg-mark.min.js');
+}
+
+function avt_bs_theme(){
+	$items=array();
+
+	$items['user_login']=array(
+	'render element' => 'form',
+	'path'=> drupal_get_path('theme', 'avt_bs').'/templates',
+	'template'=>'user_login',
+	'preprocess functions'=>array(
+	'avt_bs_preprocess_user_login'
+	),
+	);
+	return $items;
+}
