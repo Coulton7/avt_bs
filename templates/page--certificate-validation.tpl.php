f4113ee05879a4abd9f7623c8886c16fcd3dc04d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-10 15:18:31 +0100

    Removing all the flexslider script from the page

--- templates/page--certificate-validation.tpl.php
+++ templates/page--certificate-validation.tpl.php
@@ -77,18 +77,6 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/solid.css" integrity="sha384-VGP9aw4WtGH/uPAOseYxZ+Vz/vaTb1ehm1bwx92Fm8dTrE+3boLfF1SpAtB1z7HW" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css" integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
-<link rel="stylesheet" href="/sites/all/libraries/flexslider/flexslider.css" type="text/css">
-<script src="/sites/all/libraries/flexslider/jquery.flexslider-min.js"></script>
-<script type="text/javascript" charset="utf-8">
-(function($){
-	$(window).load(function() {
-		$('.flexslider').flexslider({
-			controlNav: false,
-			directionNav: false
-		});
-	})
-})(jQuery);
-</script>
 <script type="text/javascript" src="/sites/all/themes/avt_bs/js/animatedcollapse.js"></script>
 <script type="text/javascript" src="/sites/all/themes/avt_bs/js/jquery.form.js "></script>
 <script type="text/javascript">
