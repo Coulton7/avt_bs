576fb20fe2ee001dba2129bf0a228265996369f7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-10 13:39:19 +0100

    Editing the action-menu size for pages to be full width (no waves)

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -137,9 +137,9 @@
 	<?php if ($page['navigation_col']): ?>
 		<div class="navigation_col">
 			<div class="action-menu text-center">
-				<div class="col-sm-2 col-xs-0 fullscreen">
+				<div class="col-sm-0 col-xs-0 fullscreen">
 				</div>
-				<div class="col-sm-10 col-sm-offset-2 col-xs-12 no-padding">
+				<div class="col-sm-12 col-sm-offset-2 col-xs-12 no-padding">
 				<?php print render($page['navigation_col']);?>
 			</div>
 			</div>
