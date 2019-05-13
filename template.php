6465400ef085912e12720a394e95a824824fb987
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-05-13 16:07:49 +0100

    Adding comment to identify what the code is for

--- template.php
+++ template.php
@@ -9,7 +9,7 @@ function avt_bs_preprocess_page(&$vars) {
     if (isset($vars['node']->type)) {
         $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
     }
-
+    //Template files based on page titles(if works move to parent theme)
     else if (isset($variables['node']->type)) {
         $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->title;
    }
