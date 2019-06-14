ca6a396d15d59e443cb3b02e186fad4a09acc97a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-06-14 10:47:18 +0100

    Updated version of animatedcollapse to see if jquery-pack is needed

--- js/animatedcollapse.js
+++ js/animatedcollapse.js
@@ -1,11 +1,20 @@
 //** Animated Collapsible DIV v2.0- (c) Dynamic Drive DHTML code library: http://www.dynamicdrive.com.
 //** May 24th, 08'- Script rewritten and updated to 2.0.
 //** June 4th, 08'- Version 2.01: Bug fix to work with jquery 1.2.6 (which changed the way attr() behaves).
+//** March 5th, 09'- Version 2.2, which adds the following:
+			//1) ontoggle($, divobj, state) event that fires each time a DIV is expanded/collapsed, including when the page 1st loads
+			//2) Ability to expand a DIV via a URL parameter string, ie: index.htm?expanddiv=jason or index.htm?expanddiv=jason,kelly
+
+//** March 9th, 09'- Version 2.2.1: Optimized ontoggle event handler slightly.
+//** July 3rd, 09'- Version 2.4, which adds the following:
+			//1) You can now insert rel="expand[divid] | collapse[divid] | toggle[divid]" inside arbitrary links to act as DIV togglers
+			//2) For image toggler links, you can insert the attributes "data-openimage" and "data-closedimage" to update its image based on the DIV state
 
 var animatedcollapse={
-divholders: {}, //structure: {div.id, div.attrs, div.$divref}
+divholders: {}, //structure: {div.id, div.attrs, div.$divref, div.$togglerimage}
 divgroups: {}, //structure: {groupname.count, groupname.lastactivedivid}
 lastactiveingroup: {}, //structure: {lastactivediv.id}
+preloadimages: [],
 
 show:function(divids){ //public method
 	if (typeof divids=="object"){
@@ -26,6 +35,8 @@ hide:function(divids){ //public method
 },
 
 toggle:function(divid){ //public method
+	if (typeof divid=="object")
+		divid=divid[0]
 	this.showhide(divid, "toggle")
 },
 
@@ -35,6 +46,8 @@ addDiv:function(divid, attrstring){ //public function
 		var attr=new RegExp(name+"=([^,]+)", "i") //get name/value config pair (ie: width=400px,)
 		return (attr.test(this.attrs) && parseInt(RegExp.$1)!=0)? RegExp.$1 : null //return value portion (string), or 0 (false) if none found
 	}
+	this.currentid=divid //keep track of current div object being manipulated (in the event of chaining)
+	return this
 },
 
 showhide:function(divid, action){
@@ -55,11 +68,24 @@ showhide:function(divid, action){
 
 slideengine:function(divid, action){
 	var $divref=this.divholders[divid].$divref
+	var $togglerimage=this.divholders[divid].$togglerimage
 	if (this.divholders[divid] && $divref.length==1){ //if this DIV exists
 		var animateSetting={height: action}
 		if ($divref.attr('fade'))
 			animateSetting.opacity=action
-		$divref.animate(animateSetting, $divref.attr('speed')? parseInt($divref.attr('speed')) : 500)
+		$divref.animate(animateSetting, $divref.attr('speed')? parseInt($divref.attr('speed')) : 500, function(){
+			if ($togglerimage){
+				$togglerimage.attr('src', ($divref.css('display')=="none")? $togglerimage.data('srcs').closed : $togglerimage.data('srcs').open)
+			}
+			if (animatedcollapse.ontoggle){
+				try{
+					animatedcollapse.ontoggle(jQuery, $divref.get(0), $divref.css('display'))
+				}
+				catch(e){
+					alert("An error exists inside your \"ontoggle\" function:\n\n"+e+"\n\nAborting execution of function.")
+				}
+			}
+		})
 		return false
 	}
 },
@@ -67,7 +93,7 @@ slideengine:function(divid, action){
 generatemap:function(){
 	var map={}
 	for (var i=0; i<arguments.length; i++){
-		if (arguments[i][1]!=null){
+		if (arguments[i][1]!=null){ //do not generate name/value pair if value is null
 			map[arguments[i][0]]=arguments[i][1]
 		}
 	}
@@ -77,6 +103,8 @@ generatemap:function(){
 init:function(){
 	var ac=this
 	jQuery(document).ready(function($){
+		animatedcollapse.ontoggle=animatedcollapse.ontoggle || null
+		var urlparamopenids=animatedcollapse.urlparamselect() //Get div ids that should be expanded based on the url (['div1','div2',etc])
 		var persistopenids=ac.getCookie('acopendivids') //Get list of div ids that should be expanded due to persistence ('div1,div2,etc')
 		var groupswithpersist=ac.getCookie('acgroupswithpersist') //Get list of group names that have 1 or more divs with "persist" attribute defined
 		if (persistopenids!=null) //if cookie isn't null (is null if first time page loads, and cookie hasnt been set yet)
@@ -84,36 +112,61 @@ init:function(){
 		groupswithpersist=(groupswithpersist==null || groupswithpersist=='nada')? [] : groupswithpersist.split(',') //Get list of groups with divs that are persisted
 		jQuery.each(ac.divholders, function(){ //loop through each collapsible DIV object
 			this.$divref=$('#'+this.id)
-			if ((this.getAttr('persist') || jQuery.inArray(this.getAttr('group'), groupswithpersist)!=-1) && persistopenids!=null){
+			if ((this.getAttr('persist') || jQuery.inArray(this.getAttr('group'), groupswithpersist)!=-1) && persistopenids!=null){ //if this div carries a user "persist" setting, or belong to a group with at least one div that does
 				var cssdisplay=(jQuery.inArray(this.id, persistopenids)!=-1)? 'block' : 'none'
 			}
 			else{
 				var cssdisplay=this.getAttr('hide')? 'none' : null
+			}
+			if (urlparamopenids[0]=="all" || jQuery.inArray(this.id, urlparamopenids)!=-1){ //if url parameter string contains the single array element "all", or this div's ID
+				cssdisplay='block' //set div to "block", overriding any other setting
+			}
+			else if (urlparamopenids[0]=="none"){
+				cssdisplay='none' //set div to "none", overriding any other setting
 			}
 			this.$divref.css(ac.generatemap(['height', this.getAttr('height')], ['display', cssdisplay]))
 			this.$divref.attr(ac.generatemap(['groupname', this.getAttr('group')], ['fade', this.getAttr('fade')], ['speed', this.getAttr('speed')]))
 			if (this.getAttr('group')){ //if this DIV has the "group" attr defined
 				var targetgroup=ac.divgroups[this.getAttr('group')] || (ac.divgroups[this.getAttr('group')]={}) //Get settings for this group, or if it no settings exist yet, create blank object to store them in
 				targetgroup.count=(targetgroup.count||0)+1 //count # of DIVs within this group
-				if (!targetgroup.lastactivedivid && this.$divref.css('display')!='none' || cssdisplay=="block") //if this DIV was open by default or should be open due to persistence
+				if (jQuery.inArray(this.id, urlparamopenids)!=-1){ //if url parameter string contains this div's ID
+					targetgroup.lastactivedivid=this.id //remember this DIV as the last "active" DIV (this DIV will be expanded). Overrides other settings
+					targetgroup.overridepersist=1 //Indicate to override persisted div that would have been expanded
+				}
+				if (!targetgroup.lastactivedivid && this.$divref.css('display')!='none' || cssdisplay=="block" && typeof targetgroup.overridepersist=="undefined") //if this DIV was open by default or should be open due to persistence
 					targetgroup.lastactivedivid=this.id //remember this DIV as the last "active" DIV (this DIV will be expanded)
 				this.$divref.css({display:'none'}) //hide any DIV that's part of said group for now
 			}
 		}) //end divholders.each
 		jQuery.each(ac.divgroups, function(){ //loop through each group
-			if (this.lastactivedivid)
-				ac.divholders[this.lastactivedivid].$divref.show() //and show last "active" DIV within each group (one that should be expanded)
+			if (this.lastactivedivid && urlparamopenids[0]!="none") //show last "active" DIV within each group (one that should be expanded), unless url param="none"
+				ac.divholders[this.lastactivedivid].$divref.show()
 		})
-		var $allcontrols=$('*[rel]').filter('[@rel^="collapse-"], [@rel^="expand-"], [@rel^="toggle-"]') //get all elements on page with rel="collapse-", "expand-" and "toggle-"
-		var controlidentifiers=/(collapse-)|(expand-)|(toggle-)/
-		$allcontrols.each(function(){
-			$(this).click(function(){
+		if (animatedcollapse.ontoggle){
+			jQuery.each(ac.divholders, function(){ //loop through each collapsible DIV object and fire ontoggle event
+				animatedcollapse.ontoggle(jQuery, this.$divref.get(0), this.$divref.css('display'))
+			})
+		}
+ 		//Parse page for links containing rel attribute
+		var $allcontrols=$('a[rel]').filter('[rel^="collapse["], [rel^="expand["], [rel^="toggle["]') //get all elements on page with rel="collapse[]", "expand[]" and "toggle[]"
+		$allcontrols.each(function(){ //loop though each control link
+			this._divids=this.getAttribute('rel').replace(/(^\w+)|(\s+)/g, "").replace(/[\[\]']/g, "") //cache value 'div1,div2,etc' within identifier[div1,div2,etc]
+			if (this.getElementsByTagName('img').length==1 && ac.divholders[this._divids]){ //if control is an image link that toggles a single DIV (must be one to one to update status image)
+				animatedcollapse.preloadimage(this.getAttribute('data-openimage'), this.getAttribute('data-closedimage')) //preload control images (if defined)
+				$togglerimage=$(this).find('img').eq(0).data('srcs', {open:this.getAttribute('data-openimage'), closed:this.getAttribute('data-closedimage')}) //remember open and closed images' paths
+				ac.divholders[this._divids].$togglerimage=$(this).find('img').eq(0) //save reference to toggler image (to be updated inside slideengine()
+				ac.divholders[this._divids].$togglerimage.attr('src', (ac.divholders[this._divids].$divref.css('display')=="none")? $togglerimage.data('srcs').closed : $togglerimage.data('srcs').open)
+			}
+			$(this).click(function(){ //assign click behavior to each control link
 				var relattr=this.getAttribute('rel')
-				var divid=relattr.replace(controlidentifiers, '')
-				var doaction=(relattr.indexOf("collapse-")!=-1)? "hide" : (relattr.indexOf("expand-")!=-1)? "show" : "toggle"
-				return ac.showhide(divid, doaction)
+				var divids=(this._divids=="")? [] : this._divids.split(',') //convert 'div1,div2,etc' to array
+				if (divids.length>0){
+					animatedcollapse[/expand/i.test(relattr)? 'show' : /collapse/i.test(relattr)? 'hide' : 'toggle'](divids) //call corresponding public function
+					return false
+				}
 			}) //end control.click
 		})// end control.each
+
 		$(window).bind('unload', function(){
 			ac.uninit()
 		})
@@ -150,6 +203,21 @@ setCookie:function(name, value, days){
 	}
 	else //else if this is a session only cookie
 		document.cookie = name+"="+value+"; path=/"
+},
+
+urlparamselect:function(){
+	window.location.search.match(/expanddiv=([\w\-_,]+)/i) //search for expanddiv=divid or divid1,divid2,etc
+	return (RegExp.$1!="")? RegExp.$1.split(",") : []
+},
+
+preloadimage:function(){
+	var preloadimages=this.preloadimages
+	for (var i=0; i<arguments.length; i++){
+		if (arguments[i] && arguments[i].length>0){
+			preloadimages[preloadimages.length]=new Image()
+			preloadimages[preloadimages.length-1].src=arguments[i]
+		}
+	}
 }
 
 }
