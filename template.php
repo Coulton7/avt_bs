7df5b560a7096887554c9da04caa18239b0e8074
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-09-10 12:06:54 +0100

    Calling the new sticky scroll javascript

--- template.php
+++ template.php
@@ -15,6 +15,7 @@ function avt_bs_preprocess_page(&$vars) {
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/logoscroll.js');
 	drupal_add_js(drupal_get_path('theme', 'aesbs337').'/js/fade-text.js');
   drupal_add_js(drupal_get_path('theme', 'avt_bs').'/js/animatedcollapse.js');
+  drupal_add_js(drupal_get_path('theme', 'avt_bs').'/js/sticky-scroll.js');
 }
 
 function avt_bs_theme(){
