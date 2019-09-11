71dde1d958d36ceb85a5daa70ef4a5feb0d78f58
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-09-11 10:43:15 +0100

    New layout and styling for the homepage

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -201,10 +201,7 @@
   					<h1 class="page-header" id="page-main-heading"><?php print strstr($title, "®") ? str_replace("®", "<sup>®</sup>", $title) : $title; ?></h1>
 					<?php endif; ?>
 
-			<div class="<?php if (empty($page['sidebar_first']) && empty($page['sidebar_second'])) { print 'col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2'; }
-														else if (empty($page['sidebar_first']) || empty($page['sidebar_second'])) { print 'col-sm-12'; }
-														else { print 'col-sm-12'; }
-											?>">
+			<div class="col-sm-12 fullscreen section-shadow">
           <!--<div class="breadcrumb-wrap">
             <div class="container-fluid">
               <div class="row">
