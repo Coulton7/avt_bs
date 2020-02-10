7fb6ce9ead217f36f7ea6106f36dee318f6b7153
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-10 10:04:02 +0000

    Removed metatag which would work for redirecting but just refreshes page

--- templates/html--node--140.tpl.php
+++ templates/html--node--140.tpl.php
@@ -60,7 +60,6 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta http-equiv="refresh" content="3;url=https://www.avtreliability.com/certificate-validation" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
