d2ad08f9099fcecc6ae08c15bdfc2e04ce5687fd
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-05-11 13:09:50 +0100

    All changes for the theme to bceome no cdn

--- template.php
+++ template.php
@@ -18,6 +18,20 @@ function avt_bs_preprocess_page(&$vars) {
   drupal_add_js(drupal_get_path('theme', 'avt_bs').'/js/sticky-scroll.js');
 }
 
+function avt_bs_css_alter(&$css){
+  $css['sites/all/themes/avt_bs/bootstrap/css/bootstrap.min.css']['weight']=11;
+  $css['sites/all/themes/avt_bs/bootstrap/css/bootstrap-theme.min.css']['weight']=12;
+  $css['sites/all/themes/aesbs337/css/fonts-style.css']['weight']=13;
+  $css['sites/all/themes/aesbs337/css/regions-style.css']['weight']=14;
+  $css['sites/all/themes/aesbs337/css/block-style.css']['weight']=15;
+  $css['sites/all/themes/aesbs337/css/field-style.css']['weight']=16;
+  $css['sites/all/themes/aesbs337/css/jquery.scrolling-tabs.min.css']['weight']=17;
+  $css['sites/all/themes/avt_bs/css/avtfonts-style.css']['weight']=18;
+  $css['sites/all/themes/avt_bs/css/avtregions-style.css']['weight']=19;
+  $css['sites/all/themes/avt_bs/css/avtblock-style.css']['weight']=20;
+  $css['sites/all/themes/avt_bs/css/avtfield-style.css']['weight']=21;
+}
+
 function avt_bs_theme(){
 	$items=array();
 
